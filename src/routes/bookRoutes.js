208aa12769fe5d5b133da324eb47f7cd9b766074
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-05-27 22:18:09 -0900

    Refactor import statement for protectRoute middleware in bookRoutes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cloudinary from "../lib/cloudinary.js";
 import Book from "../models/Books.js";
-import protectRoute from "../middleware/auth.middleware.js";
+import { protectRoute } from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
